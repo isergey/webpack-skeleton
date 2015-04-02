a60a4d679d5372d02a6b4fbef16b3dac62e3b714
Sergey Dostovalov <dostovalov@gmail.com>
2015-04-02 21:43:36 +0300

    improve

--- src/scripts/components/filter/Radio.js
+++ src/scripts/components/filter/Radio.js
@@ -1,15 +1,10 @@
 'use strict';
 import React from 'react';
 import marked from 'marked';
-import Radio from './Radio';
 export default React.createClass({
   render() {
     return (
-      <div class="form-radio-select-field">
-        <Radio/>
-        <Radio/>
-        <Radio/>
-      </div>
+      <label><input type="radio" name="browser" value="1"/>1</label>
     );
   }
 });
