16406afc33e5167ec112791f394a6f8f2e9f7d39
Sergey <isergey@inbox.ru>
2015-04-29 03:44:04 +0300

    range improve

--- src/scripts/components/filter/inputs/Range.js
+++ src/scripts/components/filter/inputs/Range.js
@@ -4,10 +4,15 @@ import React from 'react';
 export default React.createClass({
   getDefaultProps() {
     return {
+      name: '',
+      initial: ['', ''],
       fromLabel: 'от',
       toLabel: 'до'
     };
   },
+  /**
+   * TODO: Добавить обработку onChnage
+   */
   render() {
     return (
       <div><span>{this.props.fromLabel}</span><input type="text"/> <span>{this.props.toLabel}</span><input type="text"/> <span></span></div>
